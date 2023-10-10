5023b1cc5f22ba302905b0e2e21f055fc92a862c
haibang <haibang1010999@gmail.com>
2023-10-10 13:32:37 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        chatchit app
+        <Button></Button>
+      </div>
+
+    </>
+      
+  )
+}
